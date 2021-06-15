498c3dbb3a079ea0f82d1363af8535509a6c901a
michaverhagen <micha.verhagen@gmail.com>
2021-06-15 13:49:00 +0200

    gh-7: adapted to new version of Weaviate

--- modules/metadata.py
+++ modules/metadata.py
@@ -1,36 +1,20 @@
 """ This modules contains general utility functions """
 
+from os import path
+import zipfile
 import json
+import requests
 
 
-def get_metadata(config: dict) -> list:
+def _read_metadata_file(filename: str, max_size: int, skip_n_papers: int) -> list:
     """ converts and returns the arxiv data set from json to a list
-
-    :param datafile: the json file location and name
-    :type datafile: str
-    :param max_size: the maximum number of papers to import, defaults to 1000000000
-    :type max_size: int, optional
-    :param skip_n_papers: the number of papers to skip, defaults to 0
-    :type skip_n_papers: int, optional
-    :return: a list of paper objects with metainfo
-    :rtype: list
     """
     data = []
     ids = set()
     i = 0
 
-    if config is None or 'data' not in config or 'metadata_file' not in config['data']:
-        return None
-
-    path = config['data']['metadata_file']
-    if 'n_papers' in config['data']:
-        max_size = config['data']['n_papers']
-    skip_n_papers = 0
-    if 'skip_n_papers' in config['data']:
-        skip_n_papers = config['data']['skip_n_papers']
-
-    print("Start loading ArXiv dataset -----------:", path)
-    with open(path, 'r') as file:
+    print("Start loading ArXiv dataset -----------:", filename)
+    with open(filename, 'r') as file:
         for line in file:
             if max_size > 0:
                 if len(data) >= max_size:
@@ -46,3 +30,39 @@ def get_metadata(config: dict) -> list:
     print('Done loading ArXiv dataset ------------: load {}, skip {}'.format(len(data),i-len(data)))
 
     return data[:max_size]
+
+
+def get_metadata(config: dict) -> list:
+    """ converts and returns the arxiv data set from json to a list
+    """
+
+    if config is None or 'data' not in config or 'metadata_file' not in config['data']:
+        return None
+
+    max_size = -1
+    if 'n_papers' in config['data']:
+        max_size = config['data']['n_papers']
+    skip = 0
+    if 'skip_n_papers' in config['data']:
+        skip = config['data']['skip_n_papers']
+
+    location = config['data']['metadata_file']
+    if path.exists(location):
+        filename = location
+    else:
+        if 'http' in location:
+            print("Downloading ArXiv dataset from --------:", location)
+            response = requests.get(location)
+            download = config['data']['metadata_dir'] + location.split('/')[-1]
+            open(download, 'wb').write(response.content)
+            if download.endswith('.zip'):
+                with zipfile.ZipFile(download, 'r') as zip_ref:
+                    if len(zip_ref.namelist()) > 0:
+                        filename = config['data']['metadata_dir'] + zip_ref.namelist()[0]
+                    zip_ref.extractall(config['data']['metadata_dir'])
+            else:
+                filename = download
+
+    result = _read_metadata_file(filename, max_size, skip)
+
+    return result
